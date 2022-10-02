a52e1b7fab052056814dbd669ebf3096519021a7
felixduc19 <daominhduc190999@gmail.com>
2022-10-02 22:19:02 +0700

    config server side rendering with apollo

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,21 +1,18 @@
+import { ApolloProvider } from "@apollo/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
+import { useApollo } from "../lib/apolloClient";
 import theme from "../theme";
 
-const apolloClient = new ApolloClient({
-    uri: process.env.NEXT_PUBLIC_API_URL_DEV,
-    cache: new InMemoryCache(),
-    credentials: "include",
-});
-
 function MyApp({ Component, pageProps }: AppProps) {
+    const apolloClient = useApollo(pageProps);
     return (
         <ApolloProvider client={apolloClient}>
             <ChakraProvider theme={theme}>
                 <Component {...pageProps} />
             </ChakraProvider>
+            ∏
         </ApolloProvider>
     );
 }
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,35 @@
 import Sidebar from "../components/Sidebar/Sidebar";
 
+import { Loading } from "../components/Loading";
+import { PostsDocument, usePostsQuery } from "../generated/graphql";
+import { addApolloState, initializeApollo } from "../lib/apolloClient";
+
 export default function Index() {
-    return <Sidebar>Hello World</Sidebar>;
+    const { data, loading } = usePostsQuery();
+    if (loading) {
+        return (
+            <Sidebar>
+                <Loading />
+            </Sidebar>
+        );
+    }
+    return (
+        <Sidebar>
+            {data?.posts.map((post) => (
+                <li>{post.title}</li>
+            ))}
+        </Sidebar>
+    );
 }
+
+export const getStaticProps = async () => {
+    const apolloClient = initializeApollo();
+
+    await apolloClient.query({
+        query: PostsDocument,
+    });
+
+    return addApolloState(apolloClient, {
+        props: {},
+    });
+};
